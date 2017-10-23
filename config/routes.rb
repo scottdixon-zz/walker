123c741a4c60ef303e26e8da83d7dea2f6e6d469
Scott Dixon <scott@howrefreshing.com.au>
2017-10-23 18:41:09 +1000

    Basic navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
   resources :walks
   resources :clients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
